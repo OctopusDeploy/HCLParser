94a4c916d3d2de74014eea704585a6deaf15e138
Matthew Casperson <matthewcasperson@gmail.com>
2018-04-10 11:14:30 +1000

    Fixed up assembly product

--- source/Solution Items/SolutionInfo.cs
+++ source/Solution Items/SolutionInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Octopus Deploy Pty. Ltd.")]
-[assembly: AssemblyProduct("Octopus Deploy Server Extensibility")]
+[assembly: AssemblyProduct("Octopus Deploy HCL Parser")]
 [assembly: AssemblyCopyright("Copyright © Octopus Deploy Pty. Ltd. 2016")]
 [assembly: AssemblyCulture("")]
 #if DEBUG
