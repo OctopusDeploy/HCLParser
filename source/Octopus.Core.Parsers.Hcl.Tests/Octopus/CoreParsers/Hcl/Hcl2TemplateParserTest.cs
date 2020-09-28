608b7ad5f461deaf28b14e40aa4e71c9024dd2fb
Matthew Casperson <matthewcasperson@gmail.com>
2020-09-29 07:42:57 +1000

    Added more tests from the HCL source project

--- source/Octopus.Core.Parsers.Hcl.Tests/Octopus/CoreParsers/Hcl/Hcl2TemplateParserTest.cs
+++ source/Octopus.Core.Parsers.Hcl.Tests/Octopus/CoreParsers/Hcl/Hcl2TemplateParserTest.cs
@@ -7,10 +7,12 @@ namespace Octopus.CoreParsers.Hcl
     [TestFixture]
     public class Hcl2TemplateParserTest : TerraformTemplateLoader
     {
+        const string HCL2TemplateSamples = "HCL2TemplateSamples";
+
         [Test]
         public void ExampleFromDocs()
         {
-            var template = TerraformLoadTemplate("hcl2examplefromdocs.tf");
+            var template = TerraformLoadTemplate("hcl2examplefromdocs.tf", HCL2TemplateSamples);
             var parsed = HclParser.HclTemplate.Parse(template);
             parsed.Children.Should().HaveCount(3);
         }
@@ -20,7 +22,7 @@ namespace Octopus.CoreParsers.Hcl
         [TestCase("hcl2forloop2.txt")]
         public void ForLoopObject(string file)
         {
-            var template = TerraformLoadTemplate(file);
+            var template = TerraformLoadTemplate(file, HCL2TemplateSamples);
             var parsed = HclParser.ForLoopObjectValue.Parse(template);
             parsed.StartBracket.Should().Be('{');
             parsed.EndBracket.Should().Be('}');
@@ -34,7 +36,7 @@ namespace Octopus.CoreParsers.Hcl
         [TestCase("hcl2forloop3.txt")]
         public void ForLoopList(string file)
         {
-            var template = TerraformLoadTemplate(file);
+            var template = TerraformLoadTemplate(file, HCL2TemplateSamples);
             var parsed = HclParser.ForLoopListValue.Parse(template);
             parsed.StartBracket.Should().Be('[');
             parsed.EndBracket.Should().Be(']');
@@ -50,7 +52,7 @@ namespace Octopus.CoreParsers.Hcl
         [TestCase("hcl2ifstatement3.txt")]
         public void IfStatement(string file)
         {
-            var template = TerraformLoadTemplate(file);
+            var template = TerraformLoadTemplate(file, HCL2TemplateSamples);
             var parsed = HclParser.IfStatement.Parse(template);
             parsed.Should().Be("if s.namespace == \"aws:ec2:vpc\"");
         }
@@ -83,6 +85,7 @@ namespace Octopus.CoreParsers.Hcl
             }
         }
 
+        [Test]
         [TestCase("hcl2example1.txt")]
         [TestCase("hcl2example2.txt")]
         [TestCase("hcl2example3.txt")]
@@ -101,7 +104,43 @@ namespace Octopus.CoreParsers.Hcl
         [TestCase("hcl2example16.txt")]
         public void GenericExamples(string file)
         {
-            var template = TerraformLoadTemplate(file);
+            var template = TerraformLoadTemplate(file, HCL2TemplateSamples);
+            var parsed = HclParser.HclTemplate.Parse(template);
+            var reprinted = parsed.ToString();
+        }
+
+        [Test]
+        [TestCase("attr.hcl")]
+        [TestCase("attr-expr.hcl")]
+        [TestCase("attr-literal.hcl")]
+        [TestCase("block-attrs.hcl")]
+        [TestCase("block-comment.hcl")]
+        [TestCase("block-empty.hcl")]
+        [TestCase("block-nested.hcl")]
+        [TestCase("complex.hcl")]
+        [TestCase("empty.hcl")]
+        [TestCase("escape-dollar.hcl")]
+        [TestCase("escape-newline.hcl")]
+        [TestCase("function-call.hcl")]
+        [TestCase("hash-comment.hcl")]
+        [TestCase("index.hcl")]
+        [TestCase("int.hcl")]
+        [TestCase("int-tmpl.hcl")]
+        [TestCase("just-interp.hcl")]
+        [TestCase("literal.hcl")]
+        [TestCase("lots-of-comments.hcl")]
+        [TestCase("slash-comment.hcl")]
+        [TestCase("splat-attr.hcl")]
+        [TestCase("splat-dot-full.hcl")]
+        [TestCase("splat-full.hcl")]
+        [TestCase("traversal-dot-index.hcl")]
+        [TestCase("traversal-dot-index-terminal.hcl")]
+        [TestCase("traversal-index.hcl")]
+        [TestCase("utf8.hcl")]
+        [TestCase("var.hcl")]
+        public void CorpusExamples(string file)
+        {
+            var template = TerraformLoadTemplate(file, "corpus");
             var parsed = HclParser.HclTemplate.Parse(template);
             var reprinted = parsed.ToString();
         }
@@ -111,7 +150,7 @@ namespace Octopus.CoreParsers.Hcl
         [TestCase("hcl2objectproperty2.txt", "vpc = object({\n    id = \"string\"\n    cidr_block = \"string\"\n    vpc = object({\n        id = \"string\"\n        cidr_block = \"string\"\n    })\n})")]
         public void ObjectProperty(string file, string result)
         {
-            var template = TerraformLoadTemplate(file);
+            var template = TerraformLoadTemplate(file, HCL2TemplateSamples);
             var parsed = HclParser.HclElementTypedObjectProperty.Parse(template);
             parsed.ToString().Should().Be(result);
         }
--- source/Octopus.Core.Parsers.Hcl.Tests/Octopus/CoreParsers/Hcl/TerraformTemplateLoader.cs
+++ source/Octopus.Core.Parsers.Hcl.Tests/Octopus/CoreParsers/Hcl/TerraformTemplateLoader.cs
@@ -5,11 +5,11 @@ namespace Octopus.CoreParsers.Hcl
 {
     public class TerraformTemplateLoader
     {
-        protected string TerraformLoadTemplate(string fileName)
+        protected string TerraformLoadTemplate(string fileName, string directory = "TemplateSamples")
         {
             var templatesPath = Path.Combine(
                 Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
-                @"Octopus\CoreParsers\Hcl\TemplateSamples");
+                @"Octopus\CoreParsers\Hcl\" + directory);
 
             return HclParser.NormalizeLineEndings(File.ReadAllText(Path.Combine(templatesPath, fileName))).Trim();
         }
