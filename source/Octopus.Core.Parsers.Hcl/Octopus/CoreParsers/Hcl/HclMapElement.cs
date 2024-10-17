c03d64e4e1019d884707c554c4e9acece53ecd4e
TeamCity <bob@octopus.com>
2024-10-17 15:02:12 +0000

    Run ReSharper code cleanup

--- source/Octopus.Core.Parsers.Hcl/Octopus/CoreParsers/Hcl/HclMapElement.cs
+++ source/Octopus.Core.Parsers.Hcl/Octopus/CoreParsers/Hcl/HclMapElement.cs
@@ -1,27 +1,26 @@
 ﻿using System.Linq;
 
-namespace Octopus.CoreParsers.Hcl
+namespace Octopus.CoreParsers.Hcl;
+
+/// <summary>
+///     Represents a map
+/// </summary>
+public class HclMapElement : HclElement
 {
-    /// <summary>
-    ///     Represents a map
-    /// </summary>
-    public class HclMapElement : HclElement
+    public override string Type => MapType;
+
+    public override string Value => ToString(-1);
+
+    public override string ToString(bool naked, int indent)
     {
-        public override string Type => MapType;
+        var indentString = GetIndent(indent);
+        var lineBreak = indent == -1 ? string.Empty : "\n";
+        var nextIndent = indent == -1 ? -1 : indent + 1;
+        var separator = indent == -1 ? ", " : "\n";
 
-        public override string Value => ToString(-1);
-
-        public override string ToString(bool naked, int indent)
-        {
-            var indentString = GetIndent(indent);
-            var lineBreak = indent == -1 ? string.Empty : "\n";
-            var nextIndent = indent == -1 ? -1 : indent + 1;
-            var separator = indent == -1 ? ", " : "\n";
-
-            return indentString + "{" + lineBreak +
-                   string.Join(separator,
-                       Children?.Select(child => child.ToString(nextIndent)) ?? Enumerable.Empty<string>()) +
-                   lineBreak + indentString + "}";
-        }
+        return indentString + "{" + lineBreak +
+               string.Join(separator,
+                   Children?.Select(child => child.ToString(nextIndent)) ?? Enumerable.Empty<string>()) +
+               lineBreak + indentString + "}";
     }
 }
