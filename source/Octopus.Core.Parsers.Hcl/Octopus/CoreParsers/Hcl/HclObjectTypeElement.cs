d9e900230570826db2faa7fc9c9f9c4d9bd4a18a
TeamCity <bob@octopus.com>
2026-08-30 15:03:36 +0000

    Run ReSharper code cleanup

--- source/Octopus.Core.Parsers.Hcl/Octopus/CoreParsers/Hcl/HclObjectTypeElement.cs
+++ source/Octopus.Core.Parsers.Hcl/Octopus/CoreParsers/Hcl/HclObjectTypeElement.cs
@@ -1,27 +1,26 @@
 ﻿using System.Linq;
 
-namespace Octopus.CoreParsers.Hcl
+namespace Octopus.CoreParsers.Hcl;
+
+/// <summary>
+///     Represents a map assigned to a property
+/// </summary>
+public class HclObjectTypeElement : HclElement
 {
-    /// <summary>
-    ///     Represents a map assigned to a property
-    /// </summary>
-    public class HclObjectTypeElement : HclElement
+    public override string Type => ObjectPropertyType;
+
+    public override string Value => ToString(-1);
+
+    public override string ToString(bool naked, int indent)
     {
-        public override string Type => ObjectPropertyType;
+        var indentString = indent == -1 ? string.Empty : GetIndent(indent);
+        var lineBreak = indent == -1 ? string.Empty : "\n";
+        var nextIndent = indent == -1 ? -1 : indent + 1;
+        var separator = indent == -1 ? ", " : "\n";
 
-        public override string Value => ToString(-1);
-
-        public override string ToString(bool naked, int indent)
-        {
-            var indentString = indent == -1 ? string.Empty : GetIndent(indent);
-            var lineBreak = indent == -1 ? string.Empty : "\n";
-            var nextIndent = indent == -1 ? -1 : indent + 1;
-            var separator = indent == -1 ? ", " : "\n";
-
-            return "object({" + lineBreak +
-                   string.Join(separator,
-                       Children?.Select(child => child.ToString(nextIndent)) ?? Enumerable.Empty<string>()) +
-                   lineBreak + indentString + "})";
-        }
+        return "object({" + lineBreak +
+               string.Join(separator,
+                   Children?.Select(child => child.ToString(nextIndent)) ?? Enumerable.Empty<string>()) +
+               lineBreak + indentString + "})";
     }
 }
