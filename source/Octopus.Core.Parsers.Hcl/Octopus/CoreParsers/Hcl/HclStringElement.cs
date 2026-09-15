16d145f59c3f0e71f11e2078682076d2d961cfd0
TeamCity <bob@octopus.com>
2026-09-15 15:03:09 +0000

    Run ReSharper code cleanup

--- source/Octopus.Core.Parsers.Hcl/Octopus/CoreParsers/Hcl/HclStringElement.cs
+++ source/Octopus.Core.Parsers.Hcl/Octopus/CoreParsers/Hcl/HclStringElement.cs
@@ -1,25 +1,24 @@
-﻿namespace Octopus.CoreParsers.Hcl
+﻿namespace Octopus.CoreParsers.Hcl;
+
+/// <summary>
+///     Represents a string
+/// </summary>
+public class HclStringElement : HclElement
 {
-    /// <summary>
-    ///     Represents a string
-    /// </summary>
-    public class HclStringElement : HclElement
+    public override string Type => StringType;
+
+    public override string ToString(bool naked, int indent)
     {
-        public override string Type => StringType;
+        /*
+         * ToString() is designed to return the HCL representation of this element. Naked was an older option
+         * that was used to indicate that only the Value was to be returned. For string elements, naked meant
+         * to return the value without any quotes.
+         *
+         * It is better to use the Value property for this use case, but this logic is retained for compatibility.
+         */
+        if (naked) return ProcessedValue;
 
-        public override string ToString(bool naked, int indent)
-        {
-            /*
-             * ToString() is designed to return the HCL representation of this element. Naked was an older option
-             * that was used to indicate that only the Value was to be returned. For string elements, naked meant
-             * to return the value without any quotes.
-             *
-             * It is better to use the Value property for this use case, but this logic is retained for compatibility.
-             */
-            if (naked) return ProcessedValue;
-
-            var indentString = GetIndent(indent);
-            return indentString + "\"" + EscapeQuotes(ProcessedValue) + "\"";
-        }
+        var indentString = GetIndent(indent);
+        return indentString + "\"" + EscapeQuotes(ProcessedValue) + "\"";
     }
 }
