478b25f8c89cb7c1a7b97e461a55400adf19acd6
TeamCity <bob@octopus.com>
2026-09-26 15:02:37 +0000

    Run ReSharper code cleanup

--- source/Octopus.Core.Parsers.Hcl/Octopus/CoreParsers/Hcl/HclHereDocPropertyElement.cs
+++ source/Octopus.Core.Parsers.Hcl/Octopus/CoreParsers/Hcl/HclHereDocPropertyElement.cs
@@ -1,20 +1,19 @@
-﻿namespace Octopus.CoreParsers.Hcl
+﻿namespace Octopus.CoreParsers.Hcl;
+
+/// <summary><![CDATA[
+/// Represents the value of a heredoc string assigned to a property.
+/// Heredocs starting with << are printed as is.
+/// Heredocs starting with <<- have any leading whitespace trimmed 
+/// ]]></summary>
+public class HclHereDocPropertyElement : HclHereDocElement
 {
-    /// <summary><![CDATA[
-    /// Represents the value of a heredoc string assigned to a property.
-    /// Heredocs starting with << are printed as is.
-    /// Heredocs starting with <<- have any leading whitespace trimmed 
-    /// ]]></summary>
-    public class HclHereDocPropertyElement : HclHereDocElement
+    public override string Type => HeredocStringPropertyType;
+
+    public override string ToString(bool naked, int indent)
     {
-        public override string Type => HeredocStringPropertyType;
+        if (naked) return base.ToString(true, indent);
 
-        public override string ToString(bool naked, int indent)
-        {
-            if (naked) return base.ToString(true, indent);
-
-            var indentString = GetIndent(indent);
-            return indentString + OriginalName + " = " + base.ToString(false, 0);
-        }
+        var indentString = GetIndent(indent);
+        return indentString + OriginalName + " = " + base.ToString(false, 0);
     }
 }
